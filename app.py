30dcddfdd03ac4fe54be8b32f0e0c9ba13e28189
anjan <anjankumar@123>
2025-08-28 15:13:51 +0530

    anja

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ api_key = st.secrets["API_KEY"]
 "anjankumarcr"
 # Fetch another secret
 db_password = st.secrets["DB_PASSWORD"]
+"anajn"
 
 st.write("✅ Secrets fetched successfully!")
 st.write(f"API key length: {len(api_key)}")  # Don't print actual keys in production
